f2206a5b49a4b20410c766cd84616ab2207b8c62
blk-chrys-shamvil <blk.chrys.shamvil@gmail.com>
2025-12-14 16:29:05 +0530

    webconfig updated with deployed url

--- management/src/main/java/com/hospital/management/WebConfig.java
+++ management/src/main/java/com/hospital/management/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173"
+                            ,"https://hospital-frontend-virid.vercel.app/"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
